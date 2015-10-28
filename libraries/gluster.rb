f1955fd10d408a088f8ccaf1cac975b8d4a3e885
Thijs Houtenbos <thoutenbos@schubergphilis.com>
2015-10-28 21:22:37 +0100

    fix peer_in_volume too

--- libraries/gluster.rb
+++ libraries/gluster.rb
@@ -9,7 +9,7 @@ def node_peer?(node)
 end
 
 def peer_in_volume?(peer, volume)
-  cmd = shell_out("gluster volume status #{volume} | awk '{print $2}' | awk -F : '{print $1}' | grep #{peer} | uniq")
+  cmd = shell_out("gluster volume info #{volume} | awk '{print $2}' | awk -F : '{print $1}' | grep #{peer} | uniq")
   # cmd.stdout has a \n at the end of it
   if cmd.stdout.chomp == node
     return true
