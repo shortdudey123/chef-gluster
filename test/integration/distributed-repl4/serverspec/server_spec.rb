4cf199f42702fa0a129da007f005885ff672366f
Grant Ridder <shortdudey123@gmail.com>
2016-09-13 21:06:00 -0700

    Fix serverspec verification

--- test/integration/distributed-repl4/serverspec/server_spec.rb
+++ test/integration/distributed-repl4/serverspec/server_spec.rb
@@ -6,15 +6,15 @@ describe 'gluster::server.rb' do
   end
 
   describe command('gluster volume status') do
-    its(:stdout) { should include('Brick gluster1:/data/sdb1/gv0') }
+    its(:stdout) { should include('Brick gluster1:/data/gv0/brick') }
   end
 
   describe command('gluster volume info') do
     its(:stdout) { should include('Type: Distributed-Replicate') }
     its(:stdout) { should include('Number of Bricks: 2 x 2 = 4') }
-    its(:stdout) { should include('gluster4:/data/sdb1/gv0') }
-    its(:stdout) { should include('gluster3:/data/sdb1/gv0') }
-    its(:stdout) { should include('gluster2:/data/sdb1/gv0') }
-    its(:stdout) { should include('gluster1:/data/sdb1/gv0') }
+    its(:stdout) { should include('gluster4:/data/gv0/brick') }
+    its(:stdout) { should include('gluster3:/data/gv0/brick') }
+    its(:stdout) { should include('gluster2:/data/gv0/brick') }
+    its(:stdout) { should include('gluster1:/data/gv0/brick') }
   end
 end
