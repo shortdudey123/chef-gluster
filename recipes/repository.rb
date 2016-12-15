37a8ccb30a7bbbb807a951631af9185b36d08d69
James Le Cuirot <james.le-cuirot@yakara.com>
2016-12-15 13:26:45 +0000

    Use GPG keys on yum repositories

--- recipes/repository.rb
+++ recipes/repository.rb
@@ -48,15 +48,18 @@ when 'ubuntu'
   end
 when 'redhat', 'centos'
   # CentOS 6 and 7 have Gluster in the Storage SIG instead of a gluster hosted repo
-  repo_url = if node['platform_version'].to_i > 5
-               "http://mirror.centos.org/centos/$releasever/storage/$basearch/gluster-#{node['gluster']['version']}/"
-             else
-               "https://download.gluster.org/pub/gluster/glusterfs/#{node['gluster']['version']}/LATEST/EPEL.repo/epel-$releasever/$basearch/"
-             end
+  if node['platform_version'].to_i > 5
+    repo_url = "http://mirror.centos.org/centos/$releasever/storage/$basearch/gluster-#{node['gluster']['version']}/"
+    gpg_url = 'https://raw.githubusercontent.com/CentOS-Storage-SIG/centos-release-storage-common/master/RPM-GPG-KEY-CentOS-SIG-Storage'
+  else
+    url = "https://download.gluster.org/pub/gluster/glusterfs/#{node['gluster']['version']}/LATEST/EPEL.repo"
+    repo_url = "#{url}/epel-$releasever/$basearch/"
+    gpg_url = "#{url}/dsa.pub"
+  end
 
   yum_repository 'glusterfs' do
     baseurl repo_url
-    gpgcheck false
+    gpgkey gpg_url
     action :create
   end
 end
